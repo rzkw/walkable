0526fce55246d515bb21471e1dc3eeabf71162f3
Rizky <167944030+rzkw@users.noreply.github.com>
2025-07-22 12:20:34 +0700

    updated data/header/layout/page.tsx

--- app/data.ts
+++ app/data.ts
@@ -29,17 +29,17 @@ type SocialLink = {
 
 export const PROJECTS: Project[] = [
   {
-    name: 'Motion Primitives Pro',
+    name: 'Converting resource-constrained hardware into a web/dev server',
     description:
-      'Advanced components and templates to craft beautiful websites.',
+      'turning an old laptop into an Ubuntu/Nginx server',
     link: 'https://pro.motion-primitives.com/',
     video:
       'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
     id: 'project1',
   },
   {
-    name: 'Motion Primitives',
-    description: 'UI kit to make beautiful, animated interfaces.',
+    name: 'Exploring Virtualisation',
+    description: 'Using VMWare Fusion',
     link: 'https://motion-primitives.com/',
     video:
       'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
@@ -49,35 +49,35 @@ export const PROJECTS: Project[] = [
 
 export const WORK_EXPERIENCE: WorkExperience[] = [
   {
-    company: 'Reglazed Studio',
-    title: 'CEO',
-    start: '2024',
+    company: 'Walkable Limited Liability Company',
+    title: 'Independent IT/Cloud Support Specialist',
+    start: '2025',
     end: 'Present',
-    link: 'https://ibelick.com',
+    link: 'https://walk-llc.com',
     id: 'work1',
   },
   {
-    company: 'Freelance',
-    title: 'Design Engineer',
+    company: 'Sous Chef',
+    title: 'Adeney Milk Bar',
     start: '2022',
     end: '2024',
-    link: 'https://ibelick.com',
+    link: 'https://www.instagram.com/adeney_milkbar/?hl=en',
     id: 'work2',
   },
-  {
-    company: 'Freelance',
-    title: 'Front-end Developer',
-    start: '2017',
-    end: 'Present',
-    link: 'https://ibelick.com',
-    id: 'work3',
-  },
+  // {
+  //   company: 'Freelance',
+  //   title: 'Front-end Developer',
+  //   start: '2017',
+  //   end: 'Present',
+  //   link: 'https://ibelick.com',
+  //   id: 'work3',
+  // },
 ]
 
 export const BLOG_POSTS: BlogPost[] = [
   {
-    title: 'Exploring the Intersection of Design, AI, and Design Engineering',
-    description: 'How AI is changing the way we design',
+    title: 'How I turned an old laptop into a web/dev server',
+    description: 'Expanding my basic server admin knowledge',
     link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
     uid: 'blog-1',
   },
@@ -95,31 +95,31 @@ export const BLOG_POSTS: BlogPost[] = [
     link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
     uid: 'blog-3',
   },
-  {
-    title: 'How to Export Metadata from MDX for Next.js SEO',
-    description: 'A guide on exporting metadata from MDX files to leverage Next.js SEO features.',
-    link: '/blog/example-mdx-metadata',
-    uid: 'blog-4',
-  },
+  // {
+  //   title: 'How to Export Metadata from MDX for Next.js SEO',
+  //   description: 'A guide on exporting metadata from MDX files to leverage Next.js SEO features.',
+  //   link: '/blog/example-mdx-metadata',
+  //   uid: 'blog-4',
+  // },
 ]
 
 export const SOCIAL_LINKS: SocialLink[] = [
   {
-    label: 'Github',
-    link: 'https://github.com/ibelick',
+    label: 'GitHub',
+    link: 'https://github.com/rzkw',
   },
   {
-    label: 'Twitter',
-    link: 'https://twitter.com/ibelick',
+    label: 'Seek',
+    link: 'http://seek.com.au/profile/rizky-ramadhani-l8CSc2jM2s',
   },
   {
     label: 'LinkedIn',
-    link: 'https://www.linkedin.com/in/ibelick',
+    link: 'https://www.linkedin.com/in/rizky-ramadhani3056/',
   },
-  {
-    label: 'Instagram',
-    link: 'https://www.instagram.com/ibelick',
-  },
+  // {
+  //   label: 'Instagram',
+  //   link: 'https://www.instagram.com/ibelick',
+  // },
 ]
 
-export const EMAIL = 'your@email.com'
+export const EMAIL = 'hello@walk-llc.com'
