fd6b9adcc7bc20cb7d17e765113744980f912aa2
Rizky <167944030+rzkw@users.noreply.github.com>
2025-07-22 12:33:06 +0700

    added cloudflare config, opennextjs, wrangler cli, wrangler config, opennext config, updated package.json

---
+++ .open-next/server-functions/default/app/data.ts
@@ -0,0 +1,125 @@
+type Project = {
+  name: string
+  description: string
+  link: string
+  video: string
+  id: string
+}
+
+type WorkExperience = {
+  company: string
+  title: string
+  start: string
+  end: string
+  link: string
+  id: string
+}
+
+type BlogPost = {
+  title: string
+  description: string
+  link: string
+  uid: string
+}
+
+type SocialLink = {
+  label: string
+  link: string
+}
+
+export const PROJECTS: Project[] = [
+  {
+    name: 'Converting resource-constrained hardware into a web/dev server',
+    description:
+      'turning an old laptop into an Ubuntu/Nginx server',
+    link: 'https://pro.motion-primitives.com/',
+    video:
+      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
+    id: 'project1',
+  },
+  {
+    name: 'Exploring Virtualisation',
+    description: 'Using VMWare Fusion',
+    link: 'https://motion-primitives.com/',
+    video:
+      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
+    id: 'project2',
+  },
+]
+
+export const WORK_EXPERIENCE: WorkExperience[] = [
+  {
+    company: 'Walkable Limited Liability Company',
+    title: 'Independent IT/Cloud Support Specialist',
+    start: '2025',
+    end: 'Present',
+    link: 'https://walk-llc.com',
+    id: 'work1',
+  },
+  {
+    company: 'Sous Chef',
+    title: 'Adeney Milk Bar',
+    start: '2022',
+    end: '2024',
+    link: 'https://www.instagram.com/adeney_milkbar/?hl=en',
+    id: 'work2',
+  },
+  // {
+  //   company: 'Freelance',
+  //   title: 'Front-end Developer',
+  //   start: '2017',
+  //   end: 'Present',
+  //   link: 'https://ibelick.com',
+  //   id: 'work3',
+  // },
+]
+
+export const BLOG_POSTS: BlogPost[] = [
+  {
+    title: 'How I turned an old laptop into a web/dev server',
+    description: 'Expanding my basic server admin knowledge',
+    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
+    uid: 'blog-1',
+  },
+  {
+    title: 'Why I left my job to start my own company',
+    description:
+      'A deep dive into my decision to leave my job and start my own company',
+    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
+    uid: 'blog-2',
+  },
+  {
+    title: 'What I learned from my first year of freelancing',
+    description:
+      'A look back at my first year of freelancing and what I learned',
+    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
+    uid: 'blog-3',
+  },
+  // {
+  //   title: 'How to Export Metadata from MDX for Next.js SEO',
+  //   description: 'A guide on exporting metadata from MDX files to leverage Next.js SEO features.',
+  //   link: '/blog/example-mdx-metadata',
+  //   uid: 'blog-4',
+  // },
+]
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    label: 'GitHub',
+    link: 'https://github.com/rzkw',
+  },
+  {
+    label: 'Seek',
+    link: 'http://seek.com.au/profile/rizky-ramadhani-l8CSc2jM2s',
+  },
+  {
+    label: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/rizky-ramadhani3056/',
+  },
+  // {
+  //   label: 'Instagram',
+  //   link: 'https://www.instagram.com/ibelick',
+  // },
+]
+
+export const EMAIL = 'hello@walk-llc.com'
