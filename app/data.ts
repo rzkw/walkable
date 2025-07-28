d50c0e9cc30d912c10fc8e4f74c7d5961e617577
Rizky <167944030+rzkw@users.noreply.github.com>
2025-07-28 21:18:29 +0700

    updated data.ts link, footer, added new projects route

--- app/data.ts
+++ app/data.ts
@@ -40,7 +40,7 @@ export const PROJECTS: Project[] = [
   {
     name: 'Exploring Virtualisation',
     description: 'Using VMWare Fusion',
-    link: 'https://motion-primitives.com/',
+    link: '/projects/exploring-virt',
     video:
       'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
     id: 'project2',
